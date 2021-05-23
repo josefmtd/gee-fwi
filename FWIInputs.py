3da96f9f8eae0cf159e1555a82e8854b8bb7a098
Josef Matondang <admin@josefmtd.com>
2021-05-23 11:48:05 +0700

    Cleaned FWIInputs.py

--- FWIInputs.py
+++ FWIInputs.py
@@ -45,35 +45,37 @@ class FWI_GFS_GSMAP:
         """
         Calculates the GFS temperature
         """
-        self.temp = self.gfs.select('temperature_2m_above_ground') \
-                        .first().clip(self.bounds).rename('GFS_T')
+        temp_band = 'temperature_2m_above_ground'
+        self.temp = self.gfs.select(temp_band) \
+            .rename('GFS_T')
 
     def __calculate_relative_humidity(self):
         """
         Calculates the GFS relative humidity
         """
-        self.rhum = self.gfs.select('relative_humidity_2m_above_ground') \
-                .first().clip(self.bounds).rename('GFS_RH')
+        rhum_band = 'relative_humidity_2m_above_ground'
+        self.rhum = self.gfs.select(rhum_band) \
+            .rename('GFS_RH')
 
     def __calculate_wind(self):
         """
         Adds two vectors to find the wind speed scalar magnitude
         and convert from m/s to kph
         """
-        u_comp = self.gfs.select('u_component_of_wind_10m_above_ground') \
-                    .first().clip(self.bounds)
+        u_comp_band = 'u_component_of_wind_10m_above_ground'
+        v_comp_band = 'v_component_of_wind_10m_above_ground'
+        u_comp = self.gfs.select(u_comp_band)
+        v_comp = self.gfs.select(v_comp_band)
 
-        v_comp = self.gfs.select('u_component_of_wind_10m_above_ground') \
-                    .first().clip(self.bounds)
-
-        self.wind = (((u_comp ** 2 + v_comp ** 2) ** 0.5) * 3.6).rename('GFS_W')
+        wind = ((u_comp ** 2 + v_comp ** 2) ** 0.5) * 3.6
+        self.wind = wind.rename('GFS_W')
 
     def __calculate_rain_gsmap(self):
         """
         Use JAXA GSMaP to get past 24 hours rain in mm
         """
         self.rain = self.gsmap.reduce(ee.Reducer.sum()) \
-                        .clip(self.bounds).rename('GSMAP_R24H')
+                        .rename('GSMAP_R24H')
 
     def __get_fwi_inputs(self):
         """
@@ -82,23 +84,82 @@ class FWI_GFS_GSMAP:
         # Noon WIB
         utc_datetime = datetime(self.date.year, self.date.month, \
             self.date.day, hour = 5)
+        start_datetime = utc_datetime - timedelta(days = 1)
 
-        # Last 24 hours data
-        self.gsmap = ee.ImageCollection('JAXA/GPM_L3/GSMaP/v6/operational') \
-            .filterDate((utc_datetime - timedelta(days = 1)).isoformat(), \
+        image_id = f'{self.date.year}' + \
+                    f'{str(self.date.month).zfill(2)}' + \
+                    f'{str(self.date.day).zfill(2)}00F005'
+        self.gfs = ee.Image(f'NOAA/GFS0P25/{image_id}')
+
+        gsmap = 'JAXA/GPM_L3/GSMaP/v6/operational'
+        self.gsmap = ee.ImageCollection(gsmap) \
+            .filterDate(start_datetime.isoformat(), \
                 utc_datetime.isoformat()) \
             .select('hourlyPrecipRateGC')
-
-        # Latest prediction data
-        self.gfs = ee.ImageCollection('NOAA/GFS0P25') \
-            .filterDate((utc_datetime - timedelta(hours = 6)).isoformat(), \
-                utc_datetime.isoformat()) \
-            .filterMetadata('forecast_hours', 'equals', 5)
 
         self.__calculate_temperature()
         self.__calculate_relative_humidity()
         self.__calculate_wind()
         self.__calculate_rain_gsmap()
+
+    def preprocess(self, scale):
+        """
+        Resample the rasters to a scale
+
+        Parameters
+        ----------
+        scale: int
+            the scale in meters
+        Returns
+        -------
+        None
+        """
+        self.temp = self.temp.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+        self.rhum = self.rhum.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+        self.wind = self.wind.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+        self.rain = self.rain.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+    def __export_geotiff(self, image, prefix, bucket):
+        """
+        Export image as GeoTIFF
+        """
+        date_string = f'{self.date.year}' + \
+            f'_{str(self.date.month).zfill(2)}' + \
+            f'_{str(self.date.day).zfill(2)}'
+        file_name = f'{prefix}_{date_string}'
+
+        task = ee.batch.Export.image.toCloudStorage(**{
+            'image' : image,
+            'description' : file_name,
+            'bucket' : bucket,
+            'region' : self.bounds,
+            'fileFormat' : 'GeoTIFF',
+            'scale' : 1000,
+            'maxPixels' : 10e10
+        })
+
+        task.start()
+        return task
+
+    def export_inputs(self, bucket):
+        """
+        Export all inputs as GeoTIFF to a Google Cloud Storage Bucket
+        """
+        tasks = []
+
+        tasks.append(self.__export_geotiff(self.temp, 'TEMP', bucket))
+        tasks.append(self.__export_geotiff(self.rhum, 'RHUM', bucket))
+        tasks.append(self.__export_geotiff(self.wind, 'WIND', bucket))
+        tasks.append(self.__export_geotiff(self.rain, 'RAIN', bucket))
+
+        return tasks
 
     def update_fwi_inputs(self, date):
         """
@@ -143,12 +204,10 @@ class FWI_ERA5:
 
         Attributes
         ----------
-        date_time : datetime
-            the datetime in noon local time for the observation
+        date : datetime.date
+            the date for the observation
         bounds : ee.Geometry
             the boundary used to limit the ee.Image file
-        use_gsmap : boolean
-            use gsmap or ERA5 for total precipitation
         """
         self.date_time = date_time
         self.bounds = bounds
@@ -161,10 +220,8 @@ class FWI_ERA5:
         """
         Calculates the ERA5 temperature from Kelvin to Celsius
         """
-        temp = self.dataset.select('temperature_2m') \
-                    .closest(self.date_time.isoformat()) \
-                    .first().clip(self.bounds)
-        self.temp = (temp - 273.15).rename('ERA5_T')
+        temp = self.era5.select('temperature_2m') - 273.15
+        self.temp = temp.rename('ERA5_T')
 
     def __calculate_relative_humidity(self):
         """
@@ -177,25 +234,18 @@ class FWI_ERA5:
             self.__calculate_temperature()
             temp = self.temp
 
-        dew = self.dataset.select('dewpoint_temperature_2m') \
-                    .closest(self.date_time.isoformat()) \
-                    .first().clip(self.bounds) - 273.15
+        dew = self.era5.select('dewpoint_temperature_2m') - 273.15
 
-        self.rhum = 100 * (math.exp(1) ** ((17.625 * dew) / (243.04 + dew)) / \
+        rhum = 100 * (math.exp(1) ** ((17.625 * dew) / (243.04 + dew)) / \
                     math.exp(1) ** ((17.625 * temp) / (243.04 + temp)))
-        self.rhum = self.rhum.rename('ERA5_RH')
+        self.rhum = rhum.rename('ERA5_RH')
 
     def __calculate_rain(self):
         """
         Adds total_precipitation from past 24 hours and convert from m to mm
         """
-        start = self.date_time - timedelta(days = 1)
-
-        rain_24h = self.dataset.select('total_precipitation') \
-                        .filterDate(start.isoformat(), \
-                                    self.date_time.isoformat()) \
-                        .reduce(ee.Reducer.sum()) \
-                        .clip(self.bounds)
+        rain_24h = self.era5_rain.select('total_precipitation') \
+                        .reduce(ee.Reducer.sum())
 
         self.rain = (rain_24h * 1000.0).rename('ERA5_R')
 
@@ -204,52 +254,106 @@ class FWI_ERA5:
         Adds two vectors to find the wind speed scalar magnitude
         and convert from m/s to kph
         """
-        u_comp = self.dataset.select('u_component_of_wind_10m') \
-                        .closest(self.date_time.isoformat()) \
-                        .first().clip(self.bounds)
-        v_comp = self.dataset.select('v_component_of_wind_10m') \
-                        .closest(self.date_time.isoformat()) \
-                        .first().clip(self.bounds)
-        self.wind = ((u_comp ** 2 + v_comp ** 2) ** 0.5) * 3.6
-        self.wind = self.wind.rename('ERA5_W')
+        u_comp = self.era5.select('u_component_of_wind_10m')
+        v_comp = self.era5.select('v_component_of_wind_10m')
 
-    def __calculate_rain_gsmap(self):
-        """
-        Use JAXA GSMaP to get past 24 hours rain in mm
-        """
-        start = self.date_time - timedelta(days = 1)
-
-        self.rain = ee.ImageCollection("JAXA/GPM_L3/GSMaP/v6/operational") \
-                        .select('hourlyPrecipRate') \
-                        .filterDate(start.isoformat(), \
-                                    self.date_time.isoformat()) \
-                        .reduce(ee.Reducer.sum()) \
-                        .clip(self.bounds)
+        wind = ((u_comp ** 2 + v_comp ** 2) ** 0.5) * 3.6
+        self.wind = wind.rename('ERA5_W')
 
     def __get_fwi_inputs(self):
         """
         Calculate all the inputs required for FWI Calculation
         """
+        # Noon WIB
+        utc_datetime = datetime(self.date.year, self.date.month, \
+            self.date.day, hour = 5)
+        start_datetime = utc_datetime - timedelta(days = 1)
+
+        image_id = f'{self.date.year}' +
+                    f'{str(self.date.month).zfill(2)}' +
+                    f'{str(self.date.day).zfill(2)}T05'
+
+        self.era5_rain = ee.ImageCollection('ECMWF/ERA5_LAND/HOURLY')
+            .filterDate(start_datetime.isoformat(),
+                utc_datetime.isoformat())
+
+        self.era5 = ee.Image(f'ECMWF/ERA5_LAND/HOURLY/{image_id}')
+
         self.__calculate_temperature()
         self.__calculate_relative_humidity()
         self.__calculate_wind()
-        if self.use_gsmap:
-            self.__calculate_rain_gsmap()
-        else:
-            self.__calculate_rain()
+        self.__calculate_rain()
 
-    def update_fwi_inputs(self, date_time):
+    def update_fwi_inputs(self, date):
         """
         Updates the value of FWI input variables
 
         Parameters
         ----------
-        date_time : datetime
-            next date and time for observation data request
+        date : datetime.date
+            next date for observation data request
         Returns
         -------
         None
         """
-        self.dataset = ee.ImageCollection('ECMWF/ERA5_LAND/HOURLY')
-        self.date_time = date_time
+        self.date = date
         self.__get_fwi_inputs()
+
+    def preprocess(self, scale):
+        """
+        Resample the rasters to a scale
+
+        Parameters
+        ----------
+        scale: int
+            the scale in meters
+        Returns
+        -------
+        None
+        """
+        self.temp = self.temp.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+        self.rhum = self.rhum.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+        self.wind = self.wind.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+        self.rain = self.rain.resample('bicubic') \
+            .reproject(crs = 'EPSG:4326', scale = scale)
+
+    def __export_geotiff(self, image, prefix, bucket):
+        """
+        Export image as GeoTIFF
+        """
+        date_string = f'{self.date.year}' + \
+            f'_{str(self.date.month).zfill(2)}' + \
+            f'_{str(self.date.day).zfill(2)}'
+        file_name = f'{prefix}_{date_string}'
+
+        task = ee.batch.Export.image.toCloudStorage(**{
+            'image' : image,
+            'description' : file_name,
+            'bucket' : bucket,
+            'region' : self.bounds,
+            'fileFormat' : 'GeoTIFF',
+            'scale' : 1000,
+            'maxPixels' : 10e10
+        })
+
+        task.start()
+        return task
+
+    def export_inputs(self, bucket):
+        """
+        Export all inputs as GeoTIFF to a Google Cloud Storage Bucket
+        """
+        tasks = []
+
+        tasks.append(self.__export_geotiff(self.temp, 'TEMP', bucket))
+        tasks.append(self.__export_geotiff(self.rhum, 'RHUM', bucket))
+        tasks.append(self.__export_geotiff(self.wind, 'WIND', bucket))
+        tasks.append(self.__export_geotiff(self.rain, 'RAIN', bucket))
+
+        return tasks
